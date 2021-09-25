285bcd1cef7c18b2fe79b45d3d005c6ad10b889c
Khurram Saeed <kssuper007@gmail.com>
2021-09-25 18:00:57 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,86 @@
+package com.company;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int num=10;
+//        String greetings="Hello everyone so good to see you all here today.";
+//        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//	    System.out.println("number of characters in alphabet are " + txt.length());
+//        System.out.println(txt.toUpperCase());
+//        System.out.println(txt.toLowerCase());
+//        System.out.println(greetings.indexOf("y") +" "+num);
+//
+//        String x = "10";
+//        int y = 20;
+//        String z = x + y;
+//        System.out.println(z);
+//
+//        System.out.println(Math.abs(-4.7));
+//
+//        System.out.println(Math.random());
+//        int randomNum = (int)(Math.random() * 101);  // 0 to 100
+//        System.out.println(randomNum);
+//
+//        int a = 20;
+//        int s = 20;
+//        if (a >= s) {
+//            System.out.println("a is greater than s");
+//        } else{
+//            System.out.println("yello");
+//        }
+//        byte age=30;
+//        Date now=new Date();
+//        System.out.println(now);
+////        pointers
+//        Point point1=new Point(1,1);
+//        Point point2= point1;
+//        point1.x=2;
+//        System.out.println(point2)
+////        arrays
+//        int[] numbers=new int[5];
+//        numbers[0]=111;
+//        numbers[1]=23;
+//        numbers[2]=3;
+//        numbers[3]=60;
+//        numbers[4]=309;
+//        System.out.println(Arrays.toString(numbers));
+//
+//        Arrays.sort(numbers);
+//        System.out.println(Arrays.toString(numbers));
+////        switch
+//        int day = 9;
+//        switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Wrong entry");
+
+        
+        }
+    }
+}
